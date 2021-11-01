3ed6edce8c086d53d195825cbce0258267d1d3aa
Devansh Rathi <devansh_rathi@hotmail.com>
2021-11-01 17:54:01 -0400

    Working basic nodeJS application

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>");
+    res.send("<h1>ECE416 Project 2 Team 14</h1>");
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
