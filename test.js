ebee7c4fe18443b2d535d455723e5d11c317fef9
siddmitra10 <mitra30@purdue.edu>
2021-11-14 19:45:33 -0500

    updated test.js for better debugging

--- test.js
+++ test.js
@@ -36,8 +36,16 @@ const getData = async (err, data) => {
 
 ////////////
 
+async function start(){
+    const {rate, checkIngestibility, cloneRepo} = require("./APIpackages");
+    let ans = await rate("https://github.com/alfateam/a")
+    console.log(ans);
+    // only to force cloneRepo
+    ans[0] = 0.6
+    ans[1] = 0.6
+    ans[5] = 0.6
+    ////
+    if (await checkIngestibility(ans)) await cloneRepo("https://github.com/alfateam/a");
+}
 
-const rate = require("./APIpackages");
-let ans = rate("https://github.com/alfateam/a")
-console.log(ans);
-console.log(typeof ans);
+start();
