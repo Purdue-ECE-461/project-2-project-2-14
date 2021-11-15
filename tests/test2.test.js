71725cbe1ea96ef240e3742694a28b6c50581f64
Charles Gao <gao440@purdue.com>
2021-11-15 17:47:56 -0500

    resting test and package

---
+++ tests/test2.test.js
@@ -0,0 +1,9 @@
+const API = require('../APIpackages.js');
+
+describe("array lengths and values", () => {
+    // test stuff
+    test("it should filter by a search term (link)", () => {
+        // actual test
+        expect(len(scoreArray)).toBe(7);
+      });
+  });
