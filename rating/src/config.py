dea1dec7459a31d5af0707e47cb97fdd2054d6b3
DevanshRathi1 <devansh_rathi@hotmail.com>
2021-12-08 00:45:14 -0500

    bug

--- rating/src/config.py
+++ rating/src/config.py
@@ -2,5 +2,10 @@ import os
 
 # LOG_LEVEL = int(os.environ["LOG_LEVEL"])
 # LOG_FILE = open(os.environ["LOG_FILE"], "w")
-GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+try:
+    GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+except:
+    with open(".env", "r") as envf:
+        line = envf.readline()
+        GITHUB_TOKEN = line.split("=")[1]
 
